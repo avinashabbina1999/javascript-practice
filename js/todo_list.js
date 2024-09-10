a4d77d20ea56c360e28b1a41134ea73f445fe4d7
Abbina <aabbina1s@semo.edu>
2024-09-10 14:12:33 -0500

    todo list and advanced functions

--- js/todo_list.js
+++ js/todo_list.js
@@ -1,17 +1,34 @@
 const todoList = []
+const dateList = []
 
 function displayTodo(){
     let todoListHTML = '';
     for(let i=0;i<todoList.length;i++){
-        todoListHTML += `<p>${todoList[i]}</p>`;
+        todoListHTML += `<p>
+        ${todoList[i]}
+        </p>`
+        todoListHTML += `<p>
+        ${dateList[i]}
+        </p>`
+        todoListHTML += `<p>
+        <button onclick="
+        todoList.splice(${i},1);
+        dateList.splice(${i},1);
+        displayTodo();
+        " class="css-delete">DELETE</button>
+        </p>`;
     }
     const addElement = document.querySelector('.js-add-todo');
     addElement.innerHTML = todoListHTML
 }
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
+    const dueElement = document.querySelector('.js-due-date-input');
     const name = inputElement.value;
+    const date = dueElement.value;
     todoList.push(name);
+    dateList.push(date);
     inputElement.value = '';
+    dueElement.value = '';
     displayTodo();
 }
