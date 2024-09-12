6bf70dce91533b04f1e9a95586041ba9119d75b1
Abbina <aabbina1s@semo.edu>
2024-09-12 15:06:53 -0500

    added cart

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,1 +1,43 @@
-const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'))||[{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+},{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}];
+
+export function addToCart(productId){
+    let matchingItem;
+    cart.forEach((item) => {
+        if(productId === item.productId){
+            matchingItem = item;
+        }
+    });
+
+    if(matchingItem){
+        matchingItem.quantity++;
+    }else{
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+export function removeFromCart(productId){
+    const newCart = [];
+
+    cart.forEach((item) => {
+        if(item.productId !== productId){
+            newCart.push(item);
+        }
+    });
+
+    cart = newCart;
+    saveToStorage();
+}
